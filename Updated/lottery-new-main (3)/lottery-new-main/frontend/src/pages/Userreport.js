2d44cb4e6666931c573af389cde37f94d9111fc8
ArchanaCS <103047900+ArchanaCS@users.noreply.github.com>
2023-03-10 10:27:49 +0530

    commit

--- Updated/lottery-new-main (3)/lottery-new-main/frontend/src/pages/Userreport.js
+++ Updated/lottery-new-main (3)/lottery-new-main/frontend/src/pages/Userreport.js
@@ -4,9 +4,31 @@ import HeaderUser from "../components/HeaderUser";
 import { useEffect, useState } from "react";
 import config from "../config.json";
 import axios from "axios";
+import { default as ReactSelect } from "react-select";
+import Filterbox from "../components/Filterbox"
+import { components } from "react-select";
+export const colourOptions = [
+  { value: "lottname", label: "Lottery Name" },
+  { value: "Providername", label:"Provider Name" },
+];
 export default function Userreport() {
   const [userreportlist,setUserreportlist]=useState([])
   const [totalprize,setTotalprize]=useState("")
+  const [usershow,setUsershow]=useState(false)
+  const Option = (props) => {
+    return (
+      <div>
+        <components.Option {...props}>
+          <input
+            type="checkbox"
+            checked={props.isSelected}
+            onChange={() => null}
+          />{" "}
+          <label>{props.label}</label>
+        </components.Option>
+      </div>
+    );
+  };
   useEffect(()=>{
 
     let url=config.url+"userreport";
@@ -48,19 +70,34 @@ export default function Userreport() {
           // label8click={label8click}
         />
       </div>
+     
     
     <div className="Userrreport_container">
       <div className="Userreport_header">
-
-        <div className="Userreport_header_left">
+        <div className="Userreport_reactselect">
+        <ReactSelect
+          options={colourOptions}
+          // isMulti
+          // closeMenuOnSelect={false}
+          // hideSelectedOptions={false}
+          components={{
+            Option
+          }}
+          
+          // onChange={this.handleChange}
+          // allowSelectAll={true}
+          // value={this.state.optionSelected}
+        />
+        </div>
+        {/* <div className="Userreport_header_left">
           <label>Filters with multiselect</label>
         </div>
         <div className="Userreport_header_right">
           <label>Lottery name & DrawDate</label>
-        </div>
+        </div> */}
         
       </div>
-   
+      
       <div className="Userreport_box">
             <div className="Userreport_row1">
                 <div><label>Lottername: Lottery1</label></div>
